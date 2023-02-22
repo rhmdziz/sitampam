1698f60decf401797c53cd788bdbc73faec67738
rhmdziz <rahmadaziz288@gmail.com>
2023-02-22 09:37:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// menu 
+const navbarNav = document.querySelector('.navLink');
+const hamburger = document.querySelector('#menu');
+
+
+hamburger.onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// exit menu
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+
+// SLIDE
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
